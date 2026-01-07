eb2685c6c0d2eec4d1032c04af6ace8814b32a90
narahari.amireddy <narahari.amireddy@gmail.com>
2026-01-07 17:49:45 +0000

    feat: Make website dynamic with Flask

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-import os
 
-from flask import Flask, send_file
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return send_file('src/index.html')
+    return render_template('index.html')
 
-def main():
-    app.run(port=int(os.environ.get('PORT', 80)))
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
-if __name__ == "__main__":
-    main()
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
